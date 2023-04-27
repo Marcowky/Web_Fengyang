ff6de40ba82a72ad8c52d54847e3908052746134
Marco <marcowky@qq.com>
2023-04-27 10:58:06 +0800

    add client part

--- Web_Fengyang_Server/controller/UserController.go
+++ Web_Fengyang_Server/controller/UserController.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// 注册
 func Register(c *gin.Context) {
 	// 连接当前数据库
 	db := common.GetDB()
@@ -48,4 +49,65 @@ func Register(c *gin.Context) {
 		"msg":  "注册成功",
 	})
 
+}
+
+// Login 登录
+func Login(c *gin.Context) {
+	db := common.GetDB()
+
+	// 获取参数
+	var requestUser model.User
+	c.Bind(&requestUser)
+	phoneNumber := requestUser.PhoneNumber
+	password := requestUser.Password
+
+	// 数据验证
+	var user model.User
+	db.Where("phone_number =?", phoneNumber).First(&user)
+	if user.ID == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 422,
+			"msg":  "用户不存在",
+		})
+		return
+	}
+
+	// 判断密码是否正确
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 422,
+			"msg":  "密码错误",
+		})
+		return
+	}
+
+	// 发放token
+	token, err := common.ReleaseToken(user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": 500,
+			"msg":  "系统异常",
+		})
+		return
+	}
+
+	// 返回结果
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"token": token},
+		"msg":  "登录成功",
+	})
+}
+
+// GetInfo 登录后获取信息，用于测试中间件
+func GetInfo(c *gin.Context) {
+	// 获取上下文中的用户信息
+	user, _ := c.Get("user")
+
+	// 返回用户信息
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"data": gin.H{"id": user.(model.User).ID},
+		"msg":  "登录获取信息成功",
+	})
 }
--- Web_Fengyang_Server/main.go
+++ Web_Fengyang_Server/main.go
@@ -3,34 +3,25 @@ package main
 import (
 	"Web_Fengyang_Server/common"
 	"Web_Fengyang_Server/model"
+	"Web_Fengyang_Server/routes"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
-	"net/http"
+	// "net/http"
 )
 
 func main() {
 	// 获取初始化的数据库
 	db := common.InitDB()
-	db.AutoMigrate(model.TestModel{})
-
-	db.Create(&model.TestModel{TestData: 300})
-
-	var TestModel model.TestModel
-	db.First(&TestModel, "test_data = 300")
-	print(TestModel.TestData)
-
-	// db.Delete(&TestModel, 1)
-	// db.Delete(&TestModel, 3)
-	// db.Delete(&TestModel, 3)
-
+	// 迁移user数据表
+	db.AutoMigrate(model.User{})
 	// 延迟关闭数据库
 	defer db.Close()
 	// 创建路由引擎
 	r := gin.Default()
 	// 配置静态文件路径
-	r.StaticFS("/images", http.Dir("./static/images"))
+	// r.StaticFS("/images", http.Dir("./static/images"))
 	// 启动路由
-	// routes.CollectRoutes(r)
+	routes.CollectRoutes(r)
 	// 启动服务
 	panic(r.Run(":8080"))
 }
