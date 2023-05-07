32339f1ff78ad94da8f94a333a21246a3f7a5770
Marco <marcowky@qq.com>
2023-05-08 00:46:07 +0800

    添加 B站视频嵌入方法
    详见 Web_Fengyang_Client\src\views\video\Detail.vue

--- Web_Fengyang_Client/src/routes/router.js
+++ Web_Fengyang_Client/src/routes/router.js
@@ -8,6 +8,8 @@ let routes = [
     { path: "/article/detail", component: () => import("../views/article/Detail.vue") },    // 文章详情
     { path: "/article/update", component: () => import("../views/article/Update.vue") },    // 文章更新
     { path: "", component: () => import("../views/Home.vue") },                             // 主页
+    { path: "/video", component: () => import("../views/video/Detail.vue") },               // 视频
+
 ]
 
 const router = createRouter({
---
+++ Web_Fengyang_Client/src/views/video/Detail.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <iframe src="//player.bilibili.com/player.html?aid=599898202&bvid=BV1XB4y1x7T6&cid=816709977&page=1&autoplay=false&preload=metadata" scrolling="no" frameborder="no" framespacing="0" allowfullscreen="true" style="height: 450px; width: 800px;"> </iframe>
+        <!-- 这里的代码相当方便，但因为B站限制了未登录用户的清晰度，只能360P，太鸡贼啦！ -->
+    </div>
+</template>
