96b7649b9ff6183a4b9a5176ef0dad4235c6a632
Marco <marcowky@qq.com>
2023-05-19 21:45:52 +0800

    更新 前端页面与路由

--- Web_Fengyang_Client/src/routes/router.js
+++ Web_Fengyang_Client/src/routes/router.js
@@ -3,12 +3,27 @@ import { createRouter, createWebHashHistory } from "vue-router";
 let routes = [
     { path: "/user/login", component: () => import("../views/user/Login.vue") },            // 登录界面
     { path: "/user/register", component: () => import("../views/user/Register.vue") },      // 注册界面
-    { path: "/article/publish", component: () => import("../views/article/Publish.vue") },  // 发布界面
-    { path: "/article", component: () => import("../views/article/Home.vue") },             // 文章中心
-    { path: "/article/detail", component: () => import("../views/article/Detail.vue") },    // 文章详情
-    { path: "/article/update", component: () => import("../views/article/Update.vue") },    // 文章更新
-    { path: "", component: () => import("../views/Home.vue") },                             // 主页
+    // { path: "/article/publish", component: () => import("../views/article/Publish.vue") },  // 发布界面
+    // { path: "/article", component: () => import("../views/article/Home.vue") },             // 文章中心
+    // { path: "/article/detail", component: () => import("../views/article/Detail.vue") },    // 文章详情
+    // { path: "/article/update", component: () => import("../views/article/Update.vue") },    // 文章更新
+    // { path: "/oldhome", component: () => import("../views/oldHome.vue") },                  // 主页
+
     { path: "/video", component: () => import("../views/video/Detail.vue") },               // 视频
+
+
+    { path: "", component: () => import("../views/home/Home.vue") },                        // 主页
+
+    { path: "/info", component: () => import("../views/info/Home.vue") },                        // 咨询
+
+    { path: "/attraction", component: () => import("../views/attraction/Home.vue") },         // 景点
+
+    { path: "/consumption", component: () => import("../views/consumption/Home.vue") },     // 消费
+
+    { path: "/travel", component: () => import("../views/travel/Home.vue") },                // 住宿、旅游
+
+    { path: "/blog", component: () => import("../views/blog/Home.vue") },                   // 论坛主页
+    { path: "/blog/detail", component: () => import("../views/blog/Detail.vue") },          // 论坛文章详情
 
 ]
 
