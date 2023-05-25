c170091635ca30de341b43f4c133feefd454fead
城伟 张 <2982184380@qq.com>
2023-05-25 10:01:14 +0800

    更新了消费界面的布局

--- Web_Fengyang_Client/src/views/consumption/Home.vue
+++ Web_Fengyang_Client/src/views/consumption/Home.vue
@@ -3,14 +3,19 @@
     <!-- 顶部导航栏 -->
     <TopBar @select="handleSelect" />
     
-    <div style="position: absolute; top: 15%; left: 0; height: 500px; width: 100%; z-index: 1;">
+    <div style="position: absolute; top: 100px; left: 0; height: 500px; width: 100%; z-index: 1;">
         <Chart>
         </Chart>
     </div>
     
-    <div style="position: absolute; top: 70%; left: 120px; height: 20px; width: 80%; z-index: 1;">
+    <div style="position: absolute; top: 500px; left: 120px; height: 20px; width: 80%; z-index: 1;">
         <SmallBar>
         </SmallBar>
+    </div>
+
+    <div style="position: absolute; top: 1000px; left: 120px; height: 20px; width: 80%; z-index: 1;">
+        <FooterBar>
+        </FooterBar>
     </div>
     
     </div>
@@ -19,6 +24,7 @@
 <script setup>
 // 导入顶部栏
 import TopBar from "../../components/TopBar.vue"
+import FooterBar from "../../components/FooterBar.vue"
 import Chart from './Chart.vue'
 import SmallBar from './SmallBar.vue'
 
