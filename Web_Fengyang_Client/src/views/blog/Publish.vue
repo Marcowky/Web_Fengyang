195530ecd452281615abadb6879235e4dbd9d6c7
Marco <marcowky@qq.com>
2023-05-29 01:08:44 +0800

    fix: 文章头图可以上传其他格式图片

--- Web_Fengyang_Client/src/views/blog/Publish.vue
+++ Web_Fengyang_Client/src/views/blog/Publish.vue
@@ -5,7 +5,7 @@
             <rich-text-editor v-model:modelValue="addArticle.content"></rich-text-editor>
         </div>
     </div>
-    
+
     <!-- 功能栏 -->
     <el-menu class="choiceBar" @select="handleSelect">
         <el-menu-item style="color: #409EFF;" index="1">发布</el-menu-item>
@@ -126,9 +126,9 @@ const closeSubmitModal = () => {
 
 // 判断图片的格式是否符合要求
 const beforeUpload = async (data) => {
-    if (data.file.file?.type !== "image/png") {
+    if (data.file.file?.type !== "image/png" && data.file.file?.type !== "image/jpg" && data.file.file?.type !== "image/jpeg") {
         ElMessage({
-            message: "只能上传png格式的图片",
+            message: "只能上传png/jpg/jpeg格式的图片",
             type: 'error',
             offset: 80
         })
@@ -153,68 +153,68 @@ const deleteImage = () => {
 }
 
 function filterText(text) {
-  return new Promise((resolve, reject) => {
-    axios({
-      method: 'post',
-      url: '/article/filter',
-      data: {
-        article_text: text
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-        .then(function (response) {
-          // console.log(text);
-          // response.data.filteredText为形式为{"code": 200, "message": "", "data": "<p>***的，**</p>", "use_time": 2.0265579223632812e-05}
-        //   console.log(response.data.filteredText);
-          resolve(response.data.filteredText);
+    return new Promise((resolve, reject) => {
+        axios({
+            method: 'post',
+            url: '/article/filter',
+            data: {
+                article_text: text
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-        .catch(function (error) {
-        //   console.log(error);
-          reject(error);
-        });
-  });
+            .then(function (response) {
+                // console.log(text);
+                // response.data.filteredText为形式为{"code": 200, "message": "", "data": "<p>***的，**</p>", "use_time": 2.0265579223632812e-05}
+                //   console.log(response.data.filteredText);
+                resolve(response.data.filteredText);
+            })
+            .catch(function (error) {
+                //   console.log(error);
+                reject(error);
+            });
+    });
 }
 
 const submit = async () => {
-//   console.log(value)
-  // 调用filterText函数，并将addArticle对象的content属性值作为参数传递给该函数
-  filterText(addArticle.content)
-      .then(filteredText => {
-        // 获得filteredText的"data"即处理后的字段
-        const filteredText_data = JSON.parse(filteredText).data;
-        // 将原来的addArticle.content替换为filterText函数返回的过滤后的文本
-        addArticle.content = filteredText_data;
+    //   console.log(value)
+    // 调用filterText函数，并将addArticle对象的content属性值作为参数传递给该函数
+    filterText(addArticle.content)
+        .then(filteredText => {
+            // 获得filteredText的"data"即处理后的字段
+            const filteredText_data = JSON.parse(filteredText).data;
+            // 将原来的addArticle.content替换为filterText函数返回的过滤后的文本
+            addArticle.content = filteredText_data;
 
-        return axios.post("/article", {
-          category_id: parseInt(addArticle.categoryId.slice(-1)),
-          title: addArticle.title,
-          content: addArticle.content,
-          head_image: addArticle.headImage
+            return axios.post("/article", {
+                category_id: parseInt(addArticle.categoryId.slice(-1)),
+                title: addArticle.title,
+                content: addArticle.content,
+                head_image: addArticle.headImage
+            });
+        })
+        .then(res => {
+            // console.log(addArticle)
+            // console.log(res)
+            if (res.data.code == 200) {
+                ElMessage({
+                    message: res.data.msg,
+                    type: 'success',
+                    offset: 80
+                })
+                goback()
+            } else {
+                ElMessage({
+                    message: res.data.msg,
+                    type: 'error',
+                    offset: 80
+                })
+            }
+        })
+        .catch(error => {
+            // console.log(error);
         });
-      })
-      .then(res => {
-        // console.log(addArticle)
-        // console.log(res)
-        if (res.data.code == 200) {
-          ElMessage({
-            message: res.data.msg,
-            type: 'success',
-            offset: 80
-          })
-          goback()
-        } else {
-          ElMessage({
-            message: res.data.msg,
-            type: 'error',
-            offset: 80
-          })
-        }
-      })
-      .catch(error => {
-        // console.log(error);
-      });
 }
 
 
