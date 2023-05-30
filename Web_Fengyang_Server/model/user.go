b57b5110689481ff83eabb9321cb488450439e91
Marco <marcowky@qq.com>
2023-05-31 00:40:22 +0800

    refactor: 简化user代码

--- Web_Fengyang_Server/model/user.go
+++ Web_Fengyang_Server/model/user.go
@@ -4,9 +4,9 @@ package model
 
 /* model/user.go */
 type User struct {
-	ID          uint   `gorm:"primaryKey;not null;table:users_table"`
-	UserName    string `gorm:"varchar(20);not null;table:users_table"`
-	PhoneNumber string `gorm:"varchar(20);not null;unique;table:users_table"`
-	Password    string `gorm:"size:255;not null;table:users_table"`
-	UserType    string `gorm:"varchar(20);not null;table:users_table"`
+	ID          uint   `gorm:"primaryKey;not null"`
+	UserName    string `gorm:"varchar(20);not null"`
+	PhoneNumber string `gorm:"varchar(20);not null;unique"`
+	Password    string `gorm:"size:255;not null"`
+	UserType    string `gorm:"varchar(20);not null"`
 }
