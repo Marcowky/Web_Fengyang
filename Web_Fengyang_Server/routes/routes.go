2848ec6840605c74b084a58692706bb8b99d7988
Liuy <liuy2355@mail2.sysu.edu.cn>
2023-06-01 16:25:49 +0800

    实现删除blog头图的相应前后端函数及后端路由

--- Web_Fengyang_Server/routes/routes.go
+++ Web_Fengyang_Server/routes/routes.go
@@ -17,6 +17,7 @@ func CollectRoutes(r *gin.Engine) *gin.Engine {
 	{
 		imageRoutes.POST("upload", controller.UploadImage)                              // 通用图像上传
 		imageRoutes.POST("upload/rich_editor_upload", controller.RichEditorUploadImage) // 富文本编辑器内部图像上传
+		imageRoutes.POST("delete", controller.DeleteImage)                              // blog头图删除
 	}
 
 	// 用户操作
@@ -26,7 +27,7 @@ func CollectRoutes(r *gin.Engine) *gin.Engine {
 		userRoutes.POST("register", userController.Register)                          // 注册
 		userRoutes.POST("login", userController.Login)                                // 登录
 		userRoutes.GET("info", middleware.AuthMiddleware(), userController.GetMyInfo) // 获取当前用户信息
-		userRoutes.GET("briefInfo", userController.GetBriefInfo)                  // 获取文章作者简要信息
+		userRoutes.GET("briefInfo", userController.GetBriefInfo)                      // 获取文章作者简要信息
 		userRoutes.DELETE("delete", userController.Delete)                            // 删除文章
 		userRoutes.PUT("update", userController.Update)                               // 删除文章
 		userRoutes.GET("list", userController.List)                                   // 获取用户列表
@@ -36,7 +37,7 @@ func CollectRoutes(r *gin.Engine) *gin.Engine {
 	articleController := controller.NewArticleController()
 	articleRoutes := r.Group("/article")
 	{
-		articleRoutes.POST("create", middleware.AuthMiddleware(), articleController.Create)      // 发布文章
+		articleRoutes.POST("create", middleware.AuthMiddleware(), articleController.Create)   // 发布文章
 		articleRoutes.PUT("update", middleware.AuthMiddleware(), articleController.Update)    // 修改文章
 		articleRoutes.DELETE("delete", middleware.AuthMiddleware(), articleController.Delete) // 删除文章
 		articleRoutes.GET("detail", articleController.Show)                                   // 查看文章
