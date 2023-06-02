ce4506c6166dc0bcfd3e0174c6d6316e54a85fed
Marco <marcowky@qq.com>
2023-06-02 14:44:48 +0800

    refactor: 重构路由

---
+++ Web_Fengyang_Client/src/views/clientLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <TopBar />
+    <div class="mainContent">
+        <router-view />
+    </div>
+    <FooterBar />
+    <RightBar />
+</template>
+  
+<script setup>
+
+import TopBar from "../components/TopBar.vue"
+import FooterBar from "../components/FooterBar.vue"
+import RightBar from "../components/RightBar.vue"
+
+</script>
+  
+<style scoped>
+.mainContent {
+    margin-top: 100px;
+}
+</style>
+  
+  
