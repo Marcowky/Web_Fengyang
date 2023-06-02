44b2d9aa9e0e067c0dba1a3a78e09213add56b10
Marco <marcowky@qq.com>
2023-06-02 16:11:16 +0800

    delete: 删去后端的分类配置

--- Web_Fengyang_Server/model/category.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Category struct {
-	Index    string `json:"index" gorm:"type:char(36); not null"`
-	Label    string `json:"label" gorm:"type:varchar(50);not null"`
-	MainMenu string `json:"mainmenu" gorm:"type:varchar(50);not null"`
-}
