3a578e18821a03f5817159124a7fe5a3d807f4a7
Marco <marcowky@qq.com>
2023-06-04 22:59:45 +0800

    fix: 修复双层顶部栏问题

--- Web_Fengyang_Client/src/views/clientLayout.vue
+++ Web_Fengyang_Client/src/views/clientLayout.vue
@@ -12,11 +12,12 @@
 import TopBar from "../components/TopBar.vue"
 import FooterBar from "../components/FooterBar.vue"
 import RightBar from "../components/RightBar.vue"
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { ref } from 'vue';
 
 const router = useRouter();
-const show = ref(true);
+const route = useRoute()
+const show = ref(route.path!="/home");
 
 router.beforeEach((to) => {
     if (to.path.startsWith('/home')) {
