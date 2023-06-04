3941530e482c2e2ea5ca071aafb3ccf694b03948
岳栩彬 <yuexb@mail2.sysu.edu.cn>
2023-06-04 16:31:34 +0800

    加入了trafiic.vue

--- Web_Fengyang_Client/src/routes/router.js
+++ Web_Fengyang_Client/src/routes/router.js
@@ -23,8 +23,12 @@ let routes = [
                 component: () => import("../views/consumption/Home.vue")
             },
             {
-                path: "/travel",
-                component: () => import("../views/travel/Home.vue")
+                path: "/hotel",
+                component: () => import("../views/travel/hotel.vue")
+            },
+            {
+                path: "/traffic",
+                component: () => import("../views/travel/traffic.vue")
             },
             {
                 path: '/blog',
