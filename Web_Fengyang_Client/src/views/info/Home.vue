0931e632aa189874233c8f263f5a7ae32a69ecff
weigp1 <2524634086@qq.com>
2023-06-11 11:01:42 +0800

    使悬浮动画样式保持，从后端获取文章数据，尝试实现“换一批”功能

--- Web_Fengyang_Client/src/views/info/Home.vue
+++ Web_Fengyang_Client/src/views/info/Home.vue
@@ -1,65 +1,63 @@
 
 <template>
   <!-- 顶部导航栏 -->
-    <el-card style="border-radius: 20px;margin-left: 120px;margin-right: 120px;margin-top: 30px">
-      <el-row :gutter="5" style="margin-left: 30px;margin-right: 30px">
-        <el-col :span="9" :offset="2" ><div class="grid-content ep-bg-purple" />
+    <el-card style="border-radius: 20px;margin-left: 10rem;margin-right: 10rem;margin-top: 2rem">
+      <el-row :gutter="5">
+        <el-col :span="9" :offset="1" ><div class="grid-content ep-bg-purple" />
               <h>
                 最新资讯
               </h>
           <div class="arrow">
           </div>
-          <el-card class ="part">
-            <el-scrollbar height="400px" ref="carousel" class = "news">
-              <el-row v-for="v in newslist"  key= "v.value" class = "news_item">
+          <el-card shadow ="hover" class ="part">
+            <el-scrollbar height="60%" ref="carousel" class = "news">
+              <el-row v-for="(article, index) in newsList" :key="article.value" class = "news_item">
                 <div class="news_mode">
-                  <img v-bind:src="v.res" alt=" " class ="news_img"/>
+                  <img v-bind:src="article.res" alt=" " class="news_img"/>
                   <span class="news_content" @click="showdetail()">
-                  {{v.content}}
-                </span>
-                </div>
-                <div class="big_mode">
-                  <img v-bind:src="v.res" alt=" " class ="big_img"/>
-                  <span class="big_content" @click="showdetail()">
-                  {{v.content}}
+                  {{article.content}}
                 </span>
                 </div>
               </el-row>
             </el-scrollbar>
 
+
           </el-card>
         </el-col>
 
-        <el-col :span="9" :offset="2"><div class="grid-content ep-bg-purple" />
+        <el-col :span="9" :offset="3"><div class="grid-content ep-bg-purple" />
             <h>
               景区公告
             </h>
-          <el-card class="part">
-            <el-scrollbar height="200px">
-              <el-row v-for="v in warninlist" :key="v.value">
+          <el-card shadow ="hover" class="part">
+            <el-scrollbar max-height="80%">
+              <el-row v-for="v in warningList" :key="v.value">
                 <div class="list">
                   {{v.content}}
                   <router-link :to="{path: '/info/page'}"
-                               tag="button">>></router-link>
+                               tag="button" style="position:absolute;right: 1.5rem">>></router-link>
                 </div>
               </el-row>
             </el-scrollbar>
+            <div class="flex-align" @click="change()">
+              <span>换一批</span>
+            </div>
           </el-card>
 
         </el-col>
       </el-row>
-      <el-row style="margin-top: 20px;margin-bottom: 8px;margin-left: 15px">
+      <el-row style="margin-top: 20px;margin-bottom: 8px;margin-left: 1.1rem">
         <el-divider>
-          <span style="font-size: 22px;font-weight:bold;font-family: 华文楷体">丰阳 欢迎您!</span>
+          <span style="font-size: 1.5rem;font-weight:bold;font-family: 华文楷体">丰阳 欢迎您!</span>
         </el-divider>
       </el-row>
 
-      <el-row :gutter="20" style="margin-left: 30px;margin-right: 30px">
-        <el-col :span="9" :offset="2"><div class="grid-content ep-bg-purple" />
+      <el-row :gutter="20">
+        <el-col :span="9" :offset="1"><div class="grid-content ep-bg-purple" />
             <h>
               旅游攻略
             </h>
-          <el-card class="part">
+          <el-card shadow ="hover" class="part">
               <el-card v-for="v in advicelist" class="modeCard">
                   <img
                       src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
@@ -69,11 +67,11 @@
               </el-card>
           </el-card>
         </el-col>
-        <el-col :span="9" :offset="2"><div class="grid-content ep-bg-purple" />
+        <el-col :span="9" :offset="3"><div class="grid-content ep-bg-purple" />
             <h>
               假日时节
             </h>
-          <el-card class="part">
+          <el-card shadow ="hover" class="part">
             <img
                 src="../../assets/pic1.jpg"
                 style="width: 100%;height: inherit;margin-right: 10px"
@@ -89,18 +87,91 @@
 
 
 <script setup>
-import {ref} from "vue";
-import news from '../../config/news.json';
+import { ref, reactive, inject, onMounted,onUpdated } from 'vue'
 import warning from '../../config/warning.json'
-import { useRouter } from 'vue-router'
+import news from '../../config/news.json'
+import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
 
-let carousel = ref(null)
-let newslist = news.news_items
-let warninlist =warning.warning_items
-let advicelist = news.news_items
-
+// 网络请求
+const serverUrl = inject("serverUrl")
+const axios = inject("axios")
 //首先在setup中定义
 const router = useRouter()
+// 变量初始化
+let number = '0'
+const qList = news.news_items
+let newsList = reactive([])
+let warningList = news.news_items
+let advicelist = warning.warning_items
+//
+let timeStart = 0 //截取第几组的结束
+let timeEnd = 1 //默认为0组
+let group =  0 //组数
+let num = 4//一页展示list数量
+let clickNum =  0//点击次数
+
+// 挂载页面时触发
+onMounted(() => {
+  loadArticles()
+})
+
+// 按条件加载文章列表
+const loadArticles = async () => {
+  newsList = qList.slice(
+      num * timeStart,
+      num * timeEnd
+  );
+  // let res = await axios.get(`/article/list?articleType=infoArticle&keyword=""&pageNum=1&pageSize=5&categoryId=1`)
+  let res = await axios.get(`/article/list?articleType=infoArticle&pageNum=1&pageSize=5`)
+  if (res.data.code === 200) {
+    newsList.value = res.data.data.article
+    console.log(newsList)
+  }
+  else
+  {
+    console.log("wrong")
+  }
+}
+const change = async () => {
+  if (qList.length > 4 && qList.length > num) {
+    //点击的时候获取分为几组
+    listlen();
+    //每点击一次记录点击次数
+    autoIncre();
+    clear();
+    renderR();
+  }
+}
+// 计算数据的长度，共分为几组，如果不能整除则加1
+const listlen = () => {
+  let len = qList.length;
+  group = len / num;
+  if (len % num !== 0) {
+    group = parseInt(group) + 1;
+  }
+}
+//每点击一次，记录次数
+const autoIncre = () => {
+  clickNum++;
+  timeStart++;
+  timeEnd++;
+}
+//计算将点击次数和开始截取的参数清空, 如果点击此时大于当前数据的组数，则重新开始计数。
+const clear = () =>{
+  if (clickNum > group - 1) {
+    timeStart = 0;
+    timeEnd = 1;
+    clickNum = 0;
+  }
+}
+//截取当前每组的数据
+const renderR = () =>{
+  newsList = qList.slice(
+      num * timeStart,
+      num * timeEnd
+  );
+  console.log(newsList);
+}
 
 const arrowClick = (val) => {
   if(val === 'right') {
@@ -112,130 +183,123 @@ const arrowClick = (val) => {
 const showdetail = (event) => {
  router.push('/info/page')
 }
-/*const changelayout = (val) => {
-  if(val==="enter")
-  {//@mouseover="changelayout('enter')" @mouseleave="changelayout('leave')"
-    document.getElementById("news_img").style.width = "80%";//让元素隐藏
-    //document.getElementById("news_content").style.position = "absolute";
-    document.getElementById("news_content").style.left = "10px";
-    document.getElementById("news_content").style.top = "10px";
-  }
-  else
-  {
-    document.getElementById("news_img").style.width = "25%";//让元素隐藏
-    document.getElementById("news_content").style.left = "120px";
-  }
-
-}*/
 
 </script>
 
 <style lang="scss" scoped>
 h{
-  color: rgba(244, 147, 45);
-  font-size: 26px;
-  font-weight: 700;
-  margin-bottom: 20px
+  color: rgb(216, 102, 102);
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin-bottom: 1.4rem;
 }
 
 .grid-content {
   border-radius: 20px;
-  width: 800px;
+  width: 30rem;
 }
 .part{
-  height: 520px;
-  width: 600px;
+  height: 31rem;
+  width: 32rem;
   position: relative;
-  border-radius: 10px
+  border-radius: 10px;
 }
-.news{
-  left: 5px;
-  top:15px;
-}
+
 .news_mode{
   position: relative;
-  margin: 5px;
-  display: block;
-}
-.big_mode{
-  position: relative;
-  margin: 5px;
-  display: none;
-}
-.news_item:hover .news_mode{
-  display: none;
-}
-.news_item:hover .big_mode{
-  display: block;
+  margin: 0.15rem;
 }
 .news_img{
+  transition-delay:9999s;
   height: inherit;
-  width: 25%
-}
-.big_img{
-  left: 5px;
-  height: 80%;
-  width: 80%
+  width: 30%
 }
 .news_content{
-  width: 70%;
+  transition-delay:9999s;
   position: absolute;
-  left:150px;
-  top:5px;
-  font-size: 17px;
+  left:32%;
+  right: 10%;
+  bottom: 50%;
+  font-size: 0.9rem;
+  color: black;
+  background-color:white;
+  font-weight: normal;
   overflow: hidden; // 文字超长隐藏
   text-overflow:ellipsis; // 显示...
   white-space: nowrap; // 单行显示
+  z-index:2;
 }
-.big_content{
-  width: 80%;
+
+.news{
+  left:5%;
+  top:1rem;
+}
+.news:hover .news_img{
+  transition: none;
+  height: inherit;
+  width: 30%
+}
+.news:hover .news_content{
+  transition:none;
   position: absolute;
-  left:5px;
-  bottom: 55px;
-  font-size: 17px;
+  left:32%;
+  right: 10%;
+  bottom: 50%;
+  font-size: 0.9rem;
+  font-weight: normal;
+  color: black;
+  background-color:white;
+}
+.news_item:hover .news_img{
+  transition-delay:0s;
+  width: 60%;
+  height: inherit;
+}
+.news_item:hover .news_content{
+  transition-delay:0s;
+  position: absolute;
+  left: 0;
+  right: 40%;
+  bottom: 0;
+  font-size: 1rem;
   font-weight: bold;
   color: aliceblue;
   background-color: rgba(0, 0, 0, 0.5);
-  overflow: hidden; // 文字超长隐藏
-  text-overflow:ellipsis; // 显示...
-  white-space: nowrap; // 单行显示
-  z-index: 2
 }
 
 .arrow{
   display: none;
   position: absolute;
   color: rgba(220, 156, 125, 0.8);
-  right:20px;
-  bottom:10px;
-  z-index: 2
+  right:1.4rem;
+  bottom:0.6rem;
+  z-index: 2;
 }
 .news:hover .arrow{
   display: block;
 }
 
-.summary:hover{
-  font-size: 20px;
-}
-
 .list{
   width: 100%;
-  margin-left: 20px;
-  margin-bottom: 10px;
-  font-size: 18px;
-  font-weight: 550;
+  margin-left: 2rem;
+  margin-bottom: 1rem;
+  font-size: 0.9rem;
+  overflow: hidden; // 文字超长隐藏
+  text-overflow:ellipsis; // 显示...
+  white-space: nowrap; // 单行显示
 }
 .list:hover{
   transform: scale(1.02);
   color: rgba(0, 0, 255, 0.96);
+  cursor: pointer;
 }
 
 .modeCard{
   float: left;
-  width:220px;
-  height: 240px;
-  margin-left: 40px;
-  margin-bottom: 8px;
+  width:11rem;
+  height: inherit;
+  margin-left: 2.8rem;
+  margin-top: 0.5rem;
 }
 .modeCard:hover{ //鼠标悬停时激活
 transform: scale(1.05); //放大倍数
